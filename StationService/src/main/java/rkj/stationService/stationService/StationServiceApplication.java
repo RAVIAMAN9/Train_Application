d98c2519d41ff9e7da939527af729100af318e7e
amank <ravikumarjha25@gmail.com>
2024-07-08 23:21:56 +0530

    added eureka server

--- StationService/src/main/java/rkj/stationService/stationService/StationServiceApplication.java
+++ StationService/src/main/java/rkj/stationService/stationService/StationServiceApplication.java
@@ -2,13 +2,14 @@ package rkj.stationService.stationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = {"rkj.Repository.Repo","rkj.stationService.stationService",
 "rkj.clientRepo.clientRepo"})
-@EnableFeignClients
+@EnableDiscoveryClient
 public class StationServiceApplication {
 
 	@Bean
